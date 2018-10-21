dd0cfbcfb081d058d4af23cb6cc9171a83d73957
Tatiana <tanyaivanovna@gmail.com>
2018-10-21 23:08:09 +0300

    homework 2, task 4

---
+++ hw2/hw2_task2_4.py
@@ -0,0 +1,15 @@
+import random
+
+min_value = 1
+max_value = 10
+
+random_value = random.randint(min_value, max_value)
+
+while True:
+    input_value = input("Enter value from 1 till 10: ")
+    input_value = int(input_value)
+    if input_value == random_value:
+        print("You are right, random value is %d!" % random_value)
+        break
+    else:
+        print("Try again!")
