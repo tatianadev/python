27a13a5e5a5a91fc0b432e088058c64dc9fefb73
Tatiana <tanyaivanovna@gmail.com>
2018-10-23 22:35:06 +0300

    homework 2, task 2-1-1

---
+++ hw2/hw2_task2_1_1_1.py
@@ -0,0 +1,13 @@
+file_name = 'text_file.txt'
+
+# notes:
+# https://docs.python.org/3.7/tutorial/inputoutput.html#methods-of-file-objects
+# f.read() reads the file as an individual string
+# f.readline() reads a single line of the file
+# Using the syntax for line in f: allows the user to iterate over the file line by line
+# If you want to read all the lines of a file in a list you can also use list(f) or f.readlines().
+
+# read data from file and print this data
+with open(file_name, 'r') as f:
+    for line in f:
+        print(line, end='')
