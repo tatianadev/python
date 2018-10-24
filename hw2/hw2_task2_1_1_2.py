75dc3a6334c0d80f5de27c0c4b7eeea93d059010
Tatiana <tanyaivanovna@gmail.com>
2018-10-25 00:04:35 +0300

    homework 2, task 2_1_1_2, update names of variables

--- hw2/hw2_task2_1_1_2.py
+++ hw2/hw2_task2_1_1_2.py
@@ -1,11 +1,11 @@
 file_name = 'text_file.txt'
 
-total_number_of_words = 0
-number_of_words_in_line = 0
+words_count_in_line = 0
+words_count = 0
 
 with open(file_name, 'r') as f:
     for line in f:
-        number_of_words_in_line = len(line.split())
-        total_number_of_words += number_of_words_in_line
+        words_count_in_line = len(line.split())
+        words_count += words_count_in_line
 
-print("Number of words: %d" % total_number_of_words)
+print("Number of words: %d" % words_count)
