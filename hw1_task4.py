5b4ca60526ea07c6a202b95ece0571b0482c4e06
Tatiana <tanyaivanovna@gmail.com>
2018-10-26 00:51:34 +0300

    homework 1, task 4 change function

--- hw1_task4.py
+++ hw1_task4.py
@@ -4,11 +4,14 @@ def check_palindrome(original_string):
     reverted_string = original_string[-1:-length_of_string - 1:-1]
     print("Reverted string: %s" % reverted_string)
     if original_string == reverted_string:
-        print("This record is a palindrome!")
+        return True
     else:
-        print("This record is not a palindrome!")
+        return False
 
 
 input_string = input("Enter a string value: ")
 input_string = str(input_string)
-check_palindrome(input_string)
+if check_palindrome(input_string):
+    print("This record is a palindrome!")
+else:
+    print("This record is not a palindrome!")
