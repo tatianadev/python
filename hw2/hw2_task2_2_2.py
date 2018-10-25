de99713d9eeff704f59b0ca3c7d6ded512469f2c
Tatiana <tanyaivanovna@gmail.com>
2018-10-26 00:59:24 +0300

    homework 2, task 2-2-2, change function

--- hw2/hw2_task2_2_2.py
+++ hw2/hw2_task2_2_2.py
@@ -16,18 +16,22 @@ start_month = parsed_date.tm_mon
 
 
 def find_year_and_month(input_start_year, input_start_month, input_min_year, input_number_of_day):
+    final_year = None
+    final_month = None
     while input_start_year > input_min_year:
         # monthrange returns weekday of first day of the month and number of days in month.
         if monthrange(input_start_year, input_start_month)[0] == input_number_of_day:
             final_year = input_start_year
             final_month = input_start_month
-            return final_year, final_month
+            break
         else:
             if input_start_month > 1:
                 input_start_month -= 1
             else:
                 input_start_year -= 1
                 input_start_month = 12
+
+    return final_year, final_month
 
 
 # input value
@@ -45,5 +49,6 @@ else:
         if input_value == names_of_days[i]:
             number_of_day = i
             print("Number of day: %d" % number_of_day)
-            result_data = find_year_and_month(start_year, start_month, min_year, number_of_day)
-            print("Year: %d, month: %d(%s)" % (result_data[0], result_data[1], month_name[result_data[1]]))
+            found_year_and_month = find_year_and_month(start_year, start_month, min_year, number_of_day)
+            print("Year: %d, month: %d(%s)" % (found_year_and_month[0], found_year_and_month[1],
+                                               month_name[found_year_and_month[1]]))
