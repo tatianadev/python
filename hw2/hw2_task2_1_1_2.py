6746995f2bdd376ab9aef4a4fad71500c64f2315
Tatiana <tanyaivanovna@gmail.com>
2018-10-25 22:38:32 +0300

    homework 2, task 2_1_1_2

--- hw2/hw2_task2_1_1_2.py
+++ hw2/hw2_task2_1_1_2.py
@@ -1,11 +1,11 @@
 file_name = 'text_file.txt'
 
 words_count_in_line = 0
-words_count = 0
+total_words_count = 0
 
 with open(file_name, 'r') as f:
     for line in f:
         words_count_in_line = len(line.split())
-        words_count += words_count_in_line
+        total_words_count += words_count_in_line
 
-print("Number of words: %d" % words_count)
+print("Number of words: %d" % total_words_count)
