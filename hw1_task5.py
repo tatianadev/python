b28fbe73e107413a3e9ff9fad57773650059fb1d
Tatiana <tanyaivanovna@gmail.com>
2018-10-26 00:53:38 +0300

    homework 1, task 5 change function

--- hw1_task5.py
+++ hw1_task5.py
@@ -1,8 +1,8 @@
 def compare_string_values(first_str, second_str):
     if second_string in first_string:
-        print("Second string is a part of first string!")
+        return True
     else:
-        print("Second string is not a part of first string!")
+        return False
 
 
 first_string = input("Enter value for first string: ")
@@ -12,6 +12,9 @@ second_string = input("Enter value for second string: ")
 second_string = str(second_string)
 
 if len(first_string) > 0 and len(second_string) > 0:
-    compare_string_values(first_string, second_string)
+    if compare_string_values(first_string, second_string):
+        print("Second string is a part of first string!")
+    else:
+        print("Second string is not a part of first string!")
 else:
     print("Please enter not Null values")
