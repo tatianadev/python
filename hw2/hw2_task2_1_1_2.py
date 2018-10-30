c4ea9b4eec35b61ad0dc1ce102caf679bc8ed74e
Tatiana <tanyaivanovna@gmail.com>
2018-10-30 22:53:28 +0300

    homework 2, task2_1_1_2, add changes

--- hw2/hw2_task2_1_1_2.py
+++ hw2/hw2_task2_1_1_2.py
@@ -1,14 +1,13 @@
 file_name = 'text_file.txt'
 
-words_count_in_line = 0
 total_words_count = 0
 
 with open(file_name, 'r') as f:
     # то же, что и в предыдущей задаче - можно было сразу считать весь текст:
     # text = f.read()
     # total_words_count = len(text.split())
-    for line in f:
-        words_count_in_line = len(line.split())
-        total_words_count += words_count_in_line
+
+    text = f.read()
+    total_words_count = len(text.split())
 
 print("Number of words: %d" % total_words_count)
