7f9eca1c649de8cb0a0b8ddb2e1821df423c96d8
Tatiana <tanyaivanovna@gmail.com>
2018-11-15 23:17:11 +0300

    homework 5, sqlite

---
+++ hw5/persons.py
@@ -0,0 +1,38 @@
+from os.path import isfile
+import sqlite3
+
+
+def load_persons(database):
+    cursor = database.execute('SELECT name, age, profession FROM persons')
+    return {name: {'age': age, 'profession': profession} for name, age, profession in cursor}
+
+
+if not isfile('test.db'):
+    db = sqlite3.connect('test.db')
+    db.execute('CREATE TABLE persons (name TEXT PRIMARY KEY, age INT, profession TEXT)')
+    db.commit()
+else:
+    db = sqlite3.connect('test.db')
+
+persons = load_persons(db)
+print(persons)
+
+while True:
+    try:
+        name = input('Enter name: ')
+        age = int(input('Enter age: '))
+        profession = input('Enter profession: ')
+    except:
+        print('Invalid input')
+    else:
+        break
+
+if name in persons:
+    db.execute('update persons set age = ?, profession = ? where name = ?', (age, profession, name))  # do update
+else:
+    db.execute('insert into persons values(?,?,?)', (name, age, profession))  # do insert
+db.commit()
+
+persons = load_persons(db)
+print(persons)
+
